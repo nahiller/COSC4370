625cae7733e69e4714c4a3dd96283a907f637867
Nick Hiller <nahiller07@gmail.com>
2022-09-20 18:53:09 -0500

    fixed the gap on the far right side

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -19,6 +19,13 @@ int main() {
         bmpNew.set_pixel(x, yLevel, 255, 255, 255, 0);
         bmpNew.set_pixel(x, yLevel-distance, 255, 255, 255, 0);
     }
+    for(float y=0.00; y<770; y++){
+        float xLevel=0.00;
+        xLevel=4096-(((y-385)/6.0)*((y-385)/6));
+        xLevel=sqrt(xLevel);
+        xLevel*=12;
+    bmpNew.set_pixel(xLevel,y,255,255,255,0);
+    }
     bmpNew.write("output.bmp");
 
 
