fa3e0af5f2718b77a16ae53e46e363a770774194
Nick Hiller <nahiller07@gmail.com>
2022-09-20 17:34:25 -0500

    update

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -6,15 +6,15 @@ int main() {
 
     BMP bmpNew(1000,1000,false);
     bmpNew.fill_region(0, 0, 1000, 1000, 0, 0, 0, 0);
-    for(double x=0;x<769.0;x++) //bmpNew.bmp_info_header.width
+    for(float x=0.00;x<769.0;x++) //bmpNew.bmp_info_header.width
     {
-        double yLevel=0;
-        yLevel=4096-(((x)/12)*((x)/12));
+        float yLevel=0.00;
+        yLevel=4096.0-(((x)/12.0)*((x)/12.0));
         yLevel=sqrt(yLevel);
         yLevel*=6.0;
         yLevel+=384.0;
         cout<<yLevel<<endl;
-        double distance=yLevel-384.0;
+        float distance=yLevel-384.0;
         distance*=2;
         bmpNew.set_pixel(x, yLevel, 255, 255, 255, 0);
         bmpNew.set_pixel(x, yLevel-distance, 255, 255, 255, 0);
