11451b053ad3d88cbfc12216e296d3f71ef34477
Nick Hiller <nahiller07@gmail.com>
2022-09-22 12:57:23 -0500

    Updated values

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -6,7 +6,7 @@ int main() {
 
     BMP bmpNew(1000,1000,false);
     bmpNew.fill_region(0, 0, 1000, 1000, 0, 0, 0, 0);
-    for(float x=0.00;x<769.0;x++) //bmpNew.bmp_info_header.width
+    for(float x=0.001;x<769.0;x++) //bmpNew.bmp_info_header.width
     {
         float yLevel=0.00;
         yLevel=4096.0-(((x)/12.0)*((x)/12.0));
@@ -19,12 +19,12 @@ int main() {
         bmpNew.set_pixel(x, yLevel, 255, 255, 255, 0);
         bmpNew.set_pixel(x, yLevel-distance, 255, 255, 255, 0);
     }
-    for(float y=0.00; y<770; y++){
+    for(float y=0.001; y<770; y++){
         float xLevel=0.00;
         xLevel=4096-(((y-385)/6.0)*((y-385)/6));
         xLevel=sqrt(xLevel);
         xLevel*=12;
-    bmpNew.set_pixel(xLevel,y,255,255,255,0);
+        bmpNew.set_pixel(xLevel,y,255,255,255,0);
     }
     bmpNew.write("output.bmp");
 
